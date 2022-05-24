84054e384afb4cfbd5155153fccd3fa70257982a
Remi <75871744+remireems@users.noreply.github.com>
2022-05-24 15:59:17 -0700

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC8wXVB_RoNDynbM49VkglSSUIA8GFa0-k",
+  authDomain: "clone-bc0bd.firebaseapp.com",
+  projectId: "clone-bc0bd",
+  storageBucket: "clone-bc0bd.appspot.com",
+  messagingSenderId: "990087384809",
+  appId: "1:990087384809:web:4209281c943b3b8de26a78",
+  measurementId: "G-GLH9D8ZRPN"
+}
