445706cf1667824c9e04da7f541066b94fa65e6c
Remi <75871744+remireems@users.noreply.github.com>
2022-05-25 16:06:47 -0700

    added stateprovider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { StateProvider } from './StateProvider';
 
+// initialState={initialState} => what does the data layer look like in the beginning
+// reducer={reducer} => how we manipulate the data
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>    
   </React.StrictMode>
 )
 
