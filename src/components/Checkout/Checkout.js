5eb6f9563858860cabfe15f05ff069c256d98977
Remi <75871744+remireems@users.noreply.github.com>
2022-05-25 15:17:04 -0700

    code & style

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -3,7 +3,18 @@ import './Checkout.css'
 const Checkout = () => {
   return (
     <div className='checkout'>
-      Checkout
+      <div className="checkoutLeft">
+        <img className='checkoutAd' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+
+        <div>
+          <h2 className="checkoutTitle">Your shopping basket</h2>
+        </div>
+      </div>
+
+      <div className="checkoutRight">
+        <h2>The subtotal will go here</h2>
+      </div>
+
     </div>
   )
 }
