b0a7fa5c8c63b2dfb25ca338dc172ca22929e34b
Remi <75871744+remireems@users.noreply.github.com>
2022-05-24 17:42:13 -0700

    created home component, added it on appjs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className="app">
 
       <Navbar />
-      {/* home */}
+      <Home />
     </div>
   );
 }
