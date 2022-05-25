8529c5f42c6445f5cc07babe58206edd565f7782
Remi <75871744+remireems@users.noreply.github.com>
2022-05-24 19:22:13 -0700

    add props to product comp & installed npm

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,12 @@ const Home = () => {
       </div>
 
       <div className="homeRow">
-        <Product />
+        <Product 
+          title='Apple'
+          price={5.99}
+          image='https://media.istockphoto.com/photos/red-apple-with-leaf-isolated-on-white-background-picture-id185262648?b=1&k=20&m=185262648&s=170667a&w=0&h=2ouM2rkF5oBplBmZdqs3hSOdBzA4mcGNCoF2P0KUMTM='
+          rating={5}
+        />
         <Product />
       </div>
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,21 @@
 import './Product.css'
 
-const Product = () => {
+const Product = ({ id, title, price, image, rating}) => {
   return (
     <div className='product'>
       <div className="productInfo">
-        <p>Apple</p>
+        <p>{title}</p>
         <p className='productPrice'>
           <small>$</small>
-          <strong>150</strong>
+          <strong>{price}</strong>
         </p>
         <div className="productRating">
-          <p>🌟</p>
+          {Array(rating).fill().map((_, i) => (<p>🌟</p>))}
+          
         </div>
       </div>
 
-      <img src="https://media.istockphoto.com/photos/red-apple-with-leaf-isolated-on-white-background-picture-id185262648?b=1&k=20&m=185262648&s=170667a&w=0&h=2ouM2rkF5oBplBmZdqs3hSOdBzA4mcGNCoF2P0KUMTM=" alt="" />
+      <img src={image} alt="" />
 
       <button>Add to Basket</button>
 
