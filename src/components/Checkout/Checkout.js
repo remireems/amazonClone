4ff929b88e363b98af0823b9dc19e8086dd9bf4f
Remi <75871744+remireems@users.noreply.github.com>
2022-05-28 15:58:06 -0700

    firebase deployment

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -14,7 +14,7 @@ const Checkout = () => {
         <img className='checkoutAd' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
 
         <div>
-          <h3>Hello, {user.email}</h3>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkoutTitle">Your shopping basket</h2>
           {cart.map(item => (
             <CheckoutProduct 
