dfa07bcce4f41c35937fac0db0389868e4dd1fba
Remi <75871744+remireems@users.noreply.github.com>
2022-05-28 15:08:36 -0700

    add useeffect

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import Checkout from './components/Checkout/Checkout'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import SignIn from './components/SignIn/SignIn';
+import { useEffect } from 'react';
+import { auth } from './firebase';
 
 const App = () => {
+
+  useEffect(() => {
+    // will only run once when the app component loads (its like if statement in react)
+
+    auth.onAuthStateChanged(authUser => {
+      console.log('The user is >>', authUser)
+      
+      if (authUser) {
+        // the user just logged in / the user was logged in
+      } else {
+        // the user is logged out
+      }
+    })
+  }, [])
+
   return (
     <Router>
       <div className="app">
