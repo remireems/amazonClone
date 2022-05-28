2abb2f575211fa53a006859fdc536e69c630c833
Remi <75871744+remireems@users.noreply.github.com>
2022-05-28 15:38:43 -0700

    says users email

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -6,7 +6,7 @@ import './Checkout.css'
 
 const Checkout = () => {
 
-  const [{ cart }, dispatch] = useStateValue()
+  const [{ cart, user }, dispatch] = useStateValue()
 
   return (
     <div className='checkout'>
@@ -14,6 +14,7 @@ const Checkout = () => {
         <img className='checkoutAd' src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
 
         <div>
+          <h3>Hello, {user.email}</h3>
           <h2 className="checkoutTitle">Your shopping basket</h2>
           {cart.map(item => (
             <CheckoutProduct 
