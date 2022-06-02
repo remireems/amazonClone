03dd58f6e0b28b35daa3072ca043a9db0c2c4ee6
Remi <75871744+remireems@users.noreply.github.com>
2022-06-01 17:03:54 -0700

    created 2ndary navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
-import { AutoFixHigh, Search, ShoppingCart } from '@mui/icons-material'
+import { auth } from '../../firebase'
 import { Link } from 'react-router-dom'
 import { useStateValue } from '../../StateProvider'
-import { auth } from '../../firebase'
+import { Search, ShoppingCart, MenuOutlined } from '@mui/icons-material'
 import './Navbar.css'
 
 const Navbar = () => {
@@ -15,52 +15,66 @@ const Navbar = () => {
   }
 
   return (
+
     <div className='navbar'>
-
-      <Link to='/'>
-        <img className='navLogo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazonLogo" />
-      </Link>
-
-
-
-      <div className="navSearch">
-        <input
-          type="text"
-          className='navSearchInput'
-        />
-        <Search className='searchIcon' />
-      </div>
-
-      <div className="navTabs">
-
-        <Link to={!user && '/signin'} style={{ textDecoration: 'none' }}>
-          <div onClick={handleAuth} className="navTab">
-            <span className='navTabLnOne'>Hello, {!user ? 'Guest' : user.email}</span>
-            <span className='navTabLnTwo'>{user ? 'Sign Out' : 'Sign In'}</span>
-          </div>
+      <div className='navbarTop'>
+        <Link to='/'>
+          <img className='navLogo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazonLogo" />
         </Link>
-
-        <Link to='/orders'>
-          <div className="navTab">
-            <span className='navTabLnOne'>Returns</span>
-            <span className='navTabLnTwo'>& Orders</span>
-          </div>
-        </Link>
-
-
-        <div className="navTab">
-          <span className='navTabLnOne'>Your</span>
-          <span className='navTabLnTwo'>Prime</span>
+        <div className="navSearch">
+          <input
+            type="text"
+            className='navSearchInput'
+          />
+          <Search className='searchIcon' />
         </div>
 
-        <Link to='/checkout' style={{ textDecoration: 'none' }}>
-          <div className="navCartIcon">
-            <ShoppingCart />
-            <span className='navTabLnTwo cartCounter'>{cart?.length}</span>
+        <div className="navTabs">
+          <Link to={!user && '/signin'} style={{ textDecoration: 'none' }}>
+            <div onClick={handleAuth} className="navTab">
+              <span className='navTabLnOne'>Hello, {!user ? 'Guest' : user.email}</span>
+              <span className='navTabLnTwo'>{user ? 'Sign Out' : 'Sign In'}</span>
+            </div>
+          </Link>
+
+          <Link to='/orders' style={{ textDecoration: 'none' }}>
+            <div className="navTab">
+              <span className='navTabLnOne'>Returns</span>
+              <span className='navTabLnTwo'>& Orders</span>
+            </div>
+          </Link>
+
+          <div className="navTab">
+            <span className='navTabLnOne'>Your</span>
+            <span className='navTabLnTwo'>Prime</span>
           </div>
-        </Link>
 
+          <Link to='/checkout' style={{ textDecoration: 'none' }}>
+            <div className="navCartIcon">
+              <ShoppingCart />
+              <span className='navTabLnTwo cartCounter'>{cart?.length}</span>
+            </div>
+          </Link>
+        </div>
+      </div>
 
+      <div className="navbarBottom">
+        <div className="navbarBottomLeft">
+          <MenuOutlined />
+          <span>All</span>
+        </div>
+        <div className="navbarBottomMid">
+          <p>Best Sellers</p>
+          <p>Amazon Basics</p>
+          <p>Customer Service</p>
+          <p>New Releases</p>
+          <p>Prime</p>
+          <p>Today's Deals</p>
+          <p>Gift Cards</p>
+        </div>
+        <div className="navbarBottomRight">
+          <span>All-new Fire 7 Kids tablet</span>
+        </div>
       </div>
     </div>
   )
