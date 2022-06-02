7df7d6f879b18bda154199c4c5500a375e82c328
Remi <75871744+remireems@users.noreply.github.com>
2022-06-02 15:38:20 -0700

    code & style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom'
 import './Footer.css'
 
 const Footer = () => {
   return (
     <div className="footer">
-      Hello
+      <Link to='/'>
+        <img className='navLogo' src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazonLogo" />
+      </Link>
+
+      <ul className='footerUL'>
+        <li>Conditions of Use</li>
+        <li>Privacy Notice</li>
+        <li>© 1996-2022, fakeAmazon.com, Inc. or its affiliates</li>
+      </ul>
     </div>
   )
 }
