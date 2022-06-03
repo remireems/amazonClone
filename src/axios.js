717d707831e5f732b799fe5c2bb48255078ea684
Remi <75871744+remireems@users.noreply.github.com>
2022-06-02 20:11:27 -0700

    added url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const instance = axios.create({
   // API (cloud function) URL
-  baseURL: 'http://localhost:5001/clone-bc0bd/us-central1/api' // need to replace url with deployed url
+  baseURL: 'https://us-central1-clone-bc0bd.cloudfunctions.net/api'
+  // http://localhost:5001/clone-bc0bd/us-central1/api 
 })
 
 export default instance
